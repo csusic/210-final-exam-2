55426d9fbfe3d6ccd4a350cb7622376a40000e80
Christine Susic <csusic022@insite.4cd.edu>
2026-05-18 16:59:26 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 
 #include <iomanip>
 #include <string>
+#include <vector>
 #include <array>
-#include <cstdlib>
+#include <deque>
+#include <queue>
+#include <list>
+#include <random>
 #include <ctime>
 using namespace std;
 
@@ -16,8 +20,26 @@ struct CustomerNode {
 int main() {
     srand(time(0));
 
+    //names
     array<string, 8> names = {"Abby", "Bill", "Charlie", "Derek", "Ellie", "Francis", "George", "Hilda"};
+    //drink orders
     array<string, 6> drinkOrders = {"Coffee", "Iced Tea", "Americano", "Water", "Apple Juice", "Green Tea"};
+    //muffins
+    array<string, 8> muffins = {"Abby", "Bill", "Charlie", "Derek", "Ellie", "Francis", "George", "Hilda"};
+    //friendship bracelets
+    array<string, 6> bracelets = {"Coffee", "Iced Tea", "Americano", "Water", "Apple Juice"
+    //
+    array<string, 6> drinkOrders = {"Coffee", "Iced Tea", "Americano", "Water", "Apple Juice"
+
+    //initiliaze queue with 3 customers 
+    for (int i = 0; i < 3; i++) {
+        
+    }
+    
+    //10 rounds
+    for (int i = 0; i < 3; i++) {
+        
+    }
 
     return 0;
 }
