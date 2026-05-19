9a47005eb68e3327c0c67f583d8620f7f8ca322b
Christine Susic <csusic022@insite.4cd.edu>
2026-05-18 17:39:16 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 // COMSC-210 | Final Exam 2 | Christine Susic
 
+#include <iostream>
 #include <iomanip>
 #include <string>
+#include <queue>
 #include <vector>
 #include <array>
 #include <deque>
@@ -20,40 +22,47 @@ struct Customer {
 int main() {
     srand(time(0));
     
-    //linked list
-    list<Customer> drinkOrder; 
-    //deque
-    deque<Customer> muffin;
-    //vector
-    vector<Customer> bracelet;
-    //map
-    map<Customer, int> scarf;
-
     //names
     array<string, 8> names = {"Abby", "Bill", "Charlie", "Derek", "Ellie", "Francis", "George", "Hilda"};
+    
+    //linked list
+    list<Customer> drinkOrderQueue;
     //drink orders
     array<string, 6> drinkOrders = {"Coffee", "Iced Tea", "Americano", "Water", "Apple Juice", "Green Tea"};
+    
+    //deque
+    deque<Customer> muffinQueue;
     //muffins
     array<string, 8> muffins = {"Abby", "Bill", "Charlie", "Derek", "Ellie", "Francis", "George", "Hilda"};
+    
+    //vector
+    vector<Customer> braceletQueue;
     //friendship bracelets
     array<string, 6> bracelets = {"Coffee", "Iced Tea", "Americano", "Water", "Apple Juice"};
+    
+    //map
+    map<Customer, int> scarfQueue;
     //scarves
     array<string, 6> scarves = {"Coffee", "Iced Tea", "Americano", "Water", "Apple Juice"};
 
     //initiliaze queue with 3 customers 
-    for (int i = 0; i < 3; i++) {
-        //drinkOrder.push_back(drinkOrders);
-        //muffin.push_back(muffins);
-        //bracelet.push_back(bracelets);
-        //scarf.push(scarves);
-    }
+    std::queue<std::string> customerQueue;
+    customerQueue.push("Customer 1");
+    customerQueue.push("Customer 2");
+    customerQueue.push("Customer 3");
     
+    int counter = 1; //for counting
     for (int i = 0; i < 10; i++) {
-        //10 rounds, initialize queue with 3 customers
-        for (int i = 0; i < 3; i++) {
-        
+        cout << "Round " << counter++ << endl;
+        //10 rounds
             //customer at head is always served
-            //if queue is empty, no customer served 
+            if (!drinkOrderQueue.empty()) {
+                cout << "Serving: " << endl;
+            //if queue is empty, no customer served
+            } else {
+                cout << "No customer." << endl;
+            }
+        
         
             //50% probability someone will join
             if (rand() % 2 == 0) {
@@ -65,7 +74,6 @@ int main() {
             //friendship bracelet vendor with std::vector
         
             //scarf vendor with std::map
-        }
     }
 
     return 0;
